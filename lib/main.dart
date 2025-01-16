87379cfd24b159327b32c096e38bada2ae9f51d3
vishnukumar <vishnu.kumar@digio.in>
2025-01-16 20:57:05 +0530

    google login refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,18 @@
-// Copyright 2013 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// ignore_for_file: avoid_print
-
 import 'dart:async';
-import 'dart:convert' show json;
 
-import 'package:flutter/foundation.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:http/http.dart' as http;
+import 'package:logger/logger.dart';
 import 'sign_in_button.dart';
 
-/// The scopes required by this application.
-// #docregion Initialize
 const List<String> scopes = <String>[
-  "https://www.googleapis.com/auth/userinfo.profile",
-  "https://www.googleapis.com/auth/userinfo.email",
-  "https://www.googleapis.com/auth/user.phonenumbers.read",
-  "https://www.googleapis.com/auth/user.gender.read",
-  "https://www.googleapis.com/auth/user.birthday.read",
   "openid"
 ];
 
 GoogleSignIn _googleSignIn = GoogleSignIn(
   scopes: scopes,
 );
-// #enddocregion Initialize
 
 void main() {
   runApp(
@@ -40,7 +25,6 @@ void main() {
 
 /// The SignInDemo app.
 class SignInDemo extends StatefulWidget {
-  ///
   const SignInDemo({super.key});
 
   @override
@@ -49,8 +33,6 @@ class SignInDemo extends StatefulWidget {
 
 class _SignInDemoState extends State<SignInDemo> {
   GoogleSignInAccount? _currentUser;
-  bool _isAuthorized = false; // has granted permissions?
-  String _contactText = '';
 
   @override
   void initState() {
@@ -58,120 +40,19 @@ class _SignInDemoState extends State<SignInDemo> {
 
     _googleSignIn.onCurrentUserChanged
         .listen((GoogleSignInAccount? account) async {
-// #docregion CanAccessScopes
-      // In mobile, being authenticated means being authorized...
-      bool isAuthorized = account != null;
-      // However, on web...
-      if (kIsWeb && account != null) {
-        isAuthorized = await _googleSignIn.canAccessScopes(scopes);
-      }
-// #enddocregion CanAccessScopes
-
-      setState(() {
-        _currentUser = account;
-        _isAuthorized = isAuthorized;
-      });
-
-      // Now that we know that the user can access the required scopes, the app
-      // can call the REST API.
-      if (isAuthorized) {
-        unawaited(_handleGetContact(account!));
-      }
-    });
-
-    // In the web, _googleSignIn.signInSilently() triggers the One Tap UX.
-    //
-    // It is recommended by Google Identity Services to render both the One Tap UX
-    // and the Google Sign In button together to "reduce friction and improve
-    // sign-in rates" ([docs](https://developers.google.com/identity/gsi/web/guides/display-button#html)).
-    _googleSignIn.signInSilently();
-  }
-
-  // Calls the People API REST endpoint for the signed-in user to retrieve information.
-  Future<void> _handleGetContact(GoogleSignInAccount user) async {
-    setState(() {
-      _contactText = 'Loading contact info...';
-    });
-    final http.Response response = await http.get(
-      Uri.parse('https://people.googleapis.com/v1/people/me/connections'
-          '?requestMask.includeField=person.names'),
-      headers: await user.authHeaders,
-    );
-    if (response.statusCode != 200) {
-      setState(() {
-        _contactText = 'People API gave a ${response.statusCode} '
-            'response. Check logs for details.';
-      });
-      print('People API ${response.statusCode} response: ${response.body}');
-      return;
-    }
-    final Map<String, dynamic> data =
-    json.decode(response.body) as Map<String, dynamic>;
-    final String? namedContact = _pickFirstNamedContact(data);
-    setState(() {
-      if (namedContact != null) {
-        _contactText = 'I see you know $namedContact!';
-      } else {
-        _contactText = 'No contacts to display.';
-      }
+      setState(() =>_currentUser = account);
     });
   }
 
-  String? _pickFirstNamedContact(Map<String, dynamic> data) {
-    final List<dynamic>? connections = data['connections'] as List<dynamic>?;
-    final Map<String, dynamic>? contact = connections?.firstWhere(
-          (dynamic contact) => (contact as Map<Object?, dynamic>)['names'] != null,
-      orElse: () => null,
-    ) as Map<String, dynamic>?;
-    if (contact != null) {
-      final List<dynamic> names = contact['names'] as List<dynamic>;
-      final Map<String, dynamic>? name = names.firstWhere(
-            (dynamic name) =>
-        (name as Map<Object?, dynamic>)['displayName'] != null,
-        orElse: () => null,
-      ) as Map<String, dynamic>?;
-      if (name != null) {
-        return name['displayName'] as String?;
-      }
-    }
-    return null;
-  }
-
-  // This is the on-click handler for the Sign In button that is rendered by Flutter.
-  //
-  // On the web, the on-click handler of the Sign In button is owned by the JS
-  // SDK, so this method can be considered mobile only.
-  // #docregion SignIn
   Future<void> _handleSignIn() async {
     try {
       final data=await _googleSignIn.signIn();
       final auth=await data?.authentication;
-      print(data);
+      _revokeAccess();
       print(auth);
     } catch (error) {
       print(error);
     }
-  }
-  // #enddocregion SignIn
-
-  // Prompts the user to authorize `scopes`.
-  //
-  // This action is **required** in platforms that don't perform Authentication
-  // and Authorization at the same time (like the web).
-  //
-  // On the web, this must be called from an user interaction (button click).
-  // #docregion RequestScopes
-  Future<void> _handleAuthorizeScopes() async {
-    final bool isAuthorized = await _googleSignIn.requestScopes(scopes);
-    // #enddocregion RequestScopes
-    setState(() {
-      _isAuthorized = isAuthorized;
-    });
-    // #docregion RequestScopes
-    if (isAuthorized) {
-      unawaited(_handleGetContact(_currentUser!));
-    }
-    // #enddocregion RequestScopes
   }
 
   Future<void> _handleSignOut() => _googleSignIn.disconnect();
@@ -191,23 +72,6 @@ class _SignInDemoState extends State<SignInDemo> {
             subtitle: Text(user.email),
           ),
           const Text('Signed in successfully.'),
-          if (_isAuthorized) ...<Widget>[
-            // The user has Authorized all required scopes
-            Text(_contactText),
-            ElevatedButton(
-              child: const Text('REFRESH'),
-              onPressed: () => _handleGetContact(user),
-            ),
-          ],
-          if (!_isAuthorized) ...<Widget>[
-            // The user has NOT Authorized all required scopes.
-            // (Mobile users may never see this button!)
-            const Text('Additional permissions needed to read your contacts.'),
-            ElevatedButton(
-              onPressed: _handleAuthorizeScopes,
-              child: const Text('REQUEST PERMISSIONS'),
-            ),
-          ],
           ElevatedButton(
             onPressed: _handleSignOut,
             child: const Text('SIGN OUT'),
@@ -240,5 +104,27 @@ class _SignInDemoState extends State<SignInDemo> {
           constraints: const BoxConstraints.expand(),
           child: _buildBody(),
         ));
+  }
+
+  Future<void> _revokeAccess() async {
+    try {
+      final auth = await _googleSignIn.currentUser?.authentication;
+      if(auth==null) return;
+      final accessToken = auth.accessToken;
+
+      // Revoke the access token
+      final response = await Dio().post('https://oauth2.googleapis.com/revoke',
+          data: {'token': accessToken},
+          options: Options(headers: {'Content-Type': 'application/x-www-form-urlencoded'},)
+      );
+
+      if (response.statusCode == 200) {
+        print('Access token revoked successfully');
+      } else {
+        print('Failed to revoke access token: ${response.data}');
+      }
+    } catch (e) {
+      print('Error revoking access: $e');
+    }
   }
 }
